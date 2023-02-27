d1766e74bfc5f6705e8c91383bcb1ee26e7f7bbd
l3elk4 <119819542+l3elk4@users.noreply.github.com>
2023-02-28 03:00:43 +0400

    feat: it work

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,18 @@
 class GuessingGame {
-    constructor() {}
-
+        constructor() {}
     setRange(min, max) {
-
+        this.minNum = min;
+        this.maxNum = max;
     }
-
     guess() {
-
+        this.result =   Math.round((this.maxNum + this.minNum) / 2);
+        return this.result;
     }
-
     lower() {
-
+        return this.maxNum = this.result;
     }
-
     greater() {
-
+        return this.minNum = this.result;
     }
 }
-
 module.exports = GuessingGame;
